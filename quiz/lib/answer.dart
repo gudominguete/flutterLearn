a5f381931d1ef804dc3a656708cd8677b2ca5b06
Gustavo Lopes <gudominguete@gmail.com>
2022-12-24 14:14:20 -0300

    Quiz project

---
+++ quiz/lib/answer.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Answer extends StatelessWidget {
+  final Function selectHandler;
+  final String answerText;
+
+  Answer(this.selectHandler, this.answerText);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: ElevatedButton(
+          child: Text(answerText),
+          style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(Colors.green)),
+          onPressed: selectHandler),
+    );
+  }
+}
